5914dd0e296352537cfaaf63b73dcb9a80d39b82
Maxym <103531640+MfD0@users.noreply.github.com>
2023-07-29 14:30:39 +0300

    добавляє градієнт, валідацію до форми

--- src/partials/rent-pop-up.js
+++ src/partials/rent-pop-up.js
@@ -1,3 +1,10 @@
+let isValid = false;
+const minLength = 2;
+const maxLength = 30;
+const namePattern = /^[A-Za-z]+$/;
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phonePattern = /^\+?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -6,18 +13,63 @@
   };
 
   refs.openModalBtn.addEventListener("click", (event) => {
-    event.preventDefault(); // Заборонити стандартну поведінку (перезавантаження сторінки)
+    event.preventDefault(); 
+    validateInput();
     toggleModal();
   });
+
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    isValid === true && 
+    refs.modal.classList.toggle("is-hidden");  
+  }
+  
+  function submitForm(event) {
+    event.preventDefault(); 
   }
 
-  function submitForm(event) {
-    event.preventDefault(); // Заборонити стандартну поведінку (перезавантаження сторінки)
-    // Додатковий код для обробки даних форми або відправки на сервер
-    // Наприклад, ви можете використовувати AJAX для асинхронної відправки даних на сервер
-  }
+  function validateInput() {
+    const inputName = document.getElementById("user-name") 
+    const inputEmail = document.getElementById("input-email") 
+    const inputPhone = document.getElementById("input-phone") 
+
+
+    const inputNameValue = inputName.value.trim()
+    const inputEmailValue = inputEmail.value.trim()
+    const inputPhoneValue = inputPhone.value.trim()
+    
+    if (inputNameValue.length < minLength || inputEmailValue.length > maxLength || inputEmailValue.length < minLength || inputPhoneValue.length > maxLength || inputPhoneValue.length < minLength || inputNameValue.length > maxLength) {
+      isValid = false;
+    }
+    else {
+      console.log(inputNameValue)
+      isValid = true;
+    }
+
+    if (!namePattern.test(inputNameValue)) {
+      isValid = false;
+      inputName.classList.add('error-border')
+    }
+    else {
+      inputName.classList.remove('error-border')
+    }
+
+    if (!emailPattern.test(inputEmailValue)) {
+      isValid = false;
+      inputEmail.classList.add('error-border')
+    }
+     else {
+      inputEmail.classList.remove('error-border')
+    }
+
+    if (!phonePattern.test(inputPhoneValue)) {
+      isValid = false;
+      inputPhone.classList.add('error-border')
+    }
+
+    
+console.log(isValid)
+}
+
 })();
